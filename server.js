b7dab2e2e77ab19c8c9050ecd0e66a56ffee586c
Owen Zeng <owenzeng315@gmail.com>
2026-06-24 20:57:45 -0400

    Made a basic html website with node

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
@@ -6,8 +7,38 @@ const server = http.createServer((req, res) => {
     // set up header content type
     res.setHeader('Content-Type', 'text/html');
 
-    res.write('<p>Hello World</p>');
-    res.end();
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    // send an HTML file
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            // res.write(data);
+            res.end(data);
+        }
+    })
+
 });
 
 server.listen(3000, 'localhost', () => {
